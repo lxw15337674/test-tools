920e0f13a74077c2bcf8cea18983f02dbf8e84cf
weiji <weiji@qunhemail.com>
2022-11-07 20:39:58 +0800

    feat: add json diff

--- next.config.js
+++ next.config.js
@@ -10,6 +10,16 @@ module.exports = withBundleAnalyzer({
   eslint: {
     dirs: ['.'],
   },
+  experimental: {
+    // Prefer loading of ES Modules over CommonJS
+    // @link {https://nextjs.org/blog/next-11-1#es-modules-support|Blog 11.1.0}
+    // @link {https://github.com/vercel/next.js/discussions/27876|Discussion}
+    esmExternals: true,
+    // Experimental monorepo support
+    // @link {https://github.com/vercel/next.js/pull/22867|Original PR}
+    // @link {https://github.com/vercel/next.js/discussions/26420|Discussion}
+    externalDir: true,
+  },
   poweredByHeader: false,
   trailingSlash: true,
   basePath: '',
--- src/pages/compare.tsx
+++ src/pages/compare.tsx
@@ -1,5 +1,25 @@
-const Index = () => {
-  return <div>jsonComapre</div>;
-};
+import type { DiffOnMount } from '@monaco-editor/react';
+import { DiffEditor } from '@monaco-editor/react';
+import React, { useRef } from 'react';
 
-export default Index;
+function App() {
+  const diffEditorRef = useRef(null);
+  const handleEditorMount: DiffOnMount = (editor) => {
+    diffEditorRef.current = editor;
+  };
+
+  return (
+    <>
+      <DiffEditor
+        language="json"
+        onMount={handleEditorMount}
+        options={{
+          readOnly: false,
+          originalEditable: true,
+        }}
+      />
+    </>
+  );
+}
+
+export default App;
